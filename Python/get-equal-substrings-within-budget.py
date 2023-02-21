b5079dfde0fce215ddeee76ba3944bbce14e7ebb
Szymon Slominski <simon.slominski@gmail.com>
2023-02-21 11:12:04 +0100

    Update return statement
    xrange is no longer necessary + add comments to the code

--- Python/get-equal-substrings-within-budget.py
+++ Python/get-equal-substrings-within-budget.py
@@ -10,9 +10,13 @@ class Solution(object):
         :rtype: int
         """
         left = 0
-        for right in xrange(len(s)):
+        for right in range(len(s)):
             maxCost -= abs(ord(s[right])-ord(t[right]))
             if maxCost < 0:
                 maxCost += abs(ord(s[left])-ord(t[left]))
                 left += 1
-        return (right+1)-left
+        # you can not use "right" outside the loop
+        return (right+1)-left 
+        
+        #a proper return should look like this:
+        return len(s) - left
